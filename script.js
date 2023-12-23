187f1c720b1844b0c60f88e64f3e684f6fc822fc
DraRoVol <107344569+DraRoVol@users.noreply.github.com>
2023-12-23 10:21:14 +0200

    update script.js

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ let containerSize = {
   height: window.innerHeight * 0.8,
 };
 
+fetch("blocks.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Data from blocks.json:", data);
+
+    blocks.push(...data);
+
+    calculateBlockPlacement();
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
+
 function generateRandomColor() {
   const letters = "0123456789ABCDEF";
   let color = "#";
@@ -18,7 +36,14 @@ function calculateBlockPlacement() {
   const container = document.getElementById("container");
   container.innerHTML = "";
 
-  let blockCoordinates = [];
+  const blockCoordinates = calculateCoordinates();
+  displayBlocksOnUI(blockCoordinates);
+
+  calculateAndDisplayFullness(container, blockCoordinates);
+}
+
+function calculateCoordinates() {
+  const blockCoordinates = [];
 
   blocks.forEach((block, index) => {
     let placed = false;
@@ -27,26 +52,12 @@ function calculateBlockPlacement() {
     while (!placed && !rotation) {
       for (let y = 0; y <= containerSize.height - block.height; y++) {
         for (let x = 0; x <= containerSize.width - block.width; x++) {
-          const overlap = blockCoordinates.some(
-            (coord) =>
-              x < coord.right &&
-              x + block.width > coord.left &&
-              y < coord.bottom &&
-              y + block.height > coord.top
-          );
+          const overlap = checkOverlap(blockCoordinates, x, y, block);
 
-          const outsideContainer =
-            x < 0 ||
-            y < 0 ||
-            x + block.width > containerSize.width ||
-            y + block.height > containerSize.height;
+          const outsideContainer = checkOutsideContainer(x, y, block);
 
           if (!overlap && !outsideContainer) {
-            const color =
-              blockColors[`${block.width}-${block.height}`] ||
-              generateRandomColor();
-
-            blockColors[`${block.width}-${block.height}`] = color;
+            const color = getBlockColor(block);
 
             blockCoordinates.push({
               top: y,
@@ -66,16 +77,76 @@ function calculateBlockPlacement() {
       }
 
       if (!placed && !rotation) {
-        // Rotate the block and try again
-        const temp = block.width;
-        block.width = block.height;
-        block.height = temp;
+        rotateBlock(block);
         rotation = true;
       }
     }
   });
 
-  // Calculate fullness
+  return blockCoordinates;
+}
+
+function checkOverlap(coordinates, x, y, block) {
+  return coordinates.some(
+    (coord) =>
+      x < coord.right &&
+      x + block.width > coord.left &&
+      y < coord.bottom &&
+      y + block.height > coord.top
+  );
+}
+
+function checkOutsideContainer(x, y, block) {
+  return (
+    x < 0 ||
+    y < 0 ||
+    x + block.width > containerSize.width ||
+    y + block.height > containerSize.height
+  );
+}
+
+function getBlockColor(block) {
+  const dimensionKey = `${block.width}-${block.height}`;
+  const existingColor = blockColors[dimensionKey];
+
+  if (existingColor) {
+    return existingColor;
+  } else {
+    const color = generateRandomColor();
+    blockColors[dimensionKey] = color;
+    return color;
+  }
+}
+
+function rotateBlock(block) {
+  const temp = block.width;
+  block.width = block.height;
+  block.height = temp;
+}
+
+function displayBlocksOnUI(blockCoordinates) {
+  const container = document.getElementById("container");
+
+  blockCoordinates.forEach((coord) => {
+    const blockElement = createBlockElement(coord);
+    container.appendChild(blockElement);
+  });
+}
+
+function createBlockElement(coord) {
+  const blockElement = document.createElement("div");
+  blockElement.className = "block";
+  blockElement.style.width = `${coord.right - coord.left}px`;
+  blockElement.style.height = `${coord.bottom - coord.top}px`;
+  blockElement.style.top = `${coord.top}px`;
+  blockElement.style.left = `${coord.left}px`;
+  blockElement.innerText = coord.initialOrder;
+  blockElement.style.backgroundColor = coord.color;
+
+  return blockElement;
+}
+
+function calculateAndDisplayFullness(container, blockCoordinates) {
   const totalArea = containerSize.width * containerSize.height;
   const occupiedArea = blockCoordinates.reduce(
     (acc, coord) =>
@@ -87,20 +158,6 @@ function calculateBlockPlacement() {
 
   const fullnessDisplay = document.getElementById("fullnessDisplay");
   fullnessDisplay.innerText = `Fullness: ${Math.round(fullness * 100)}%`;
-
-  // Display blocks on the UI
-  blockCoordinates.forEach((coord) => {
-    const blockElement = document.createElement("div");
-    blockElement.className = "block";
-    blockElement.style.width = `${coord.right - coord.left}px`;
-    blockElement.style.height = `${coord.bottom - coord.top}px`;
-    blockElement.style.top = `${coord.top}px`;
-    blockElement.style.left = `${coord.left}px`;
-    blockElement.innerText = coord.initialOrder;
-    blockElement.style.backgroundColor = coord.color;
-
-    container.appendChild(blockElement);
-  });
 
   console.log({ fullness, blockCoordinates });
 }
@@ -125,21 +182,3 @@ window.addEventListener("resize", () => {
   };
   calculateBlockPlacement();
 });
-
-fetch("blocks.json")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`Network response was not ok: ${response.statusText}`);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log("Data from blocks.json:", data);
-
-    blocks.push(...data);
-
-    calculateBlockPlacement();
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
